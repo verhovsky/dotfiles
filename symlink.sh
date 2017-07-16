f07c5c978ebc0a3b27ec7ae07aac8109fb1b2ae6
BorisVerk <boris.verk@gmail.com>
2017-07-16 14:25:14 -0600

    don't expand ~ in echo in symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -6,8 +6,10 @@ echo
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  sudo rm -rf ~/.bashrc > /dev/null 2>&1
   sudo rm -rf ~/.nvimrc > /dev/null 2>&1
   sudo rm -rf ~/.nvim > /dev/null 2>&1
+  sudo rm -rf ~/.spacemacs > /dev/null 2>&1
   sudo rm -rf ~/.gitconfig > /dev/null 2>&1
   sudo rm -rf ~/.gitignore > /dev/null 2>&1
   sudo rm -rf ~/.wgetrc > /dev/null 2>&1
@@ -18,6 +20,7 @@ then
   sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
   sudo rm -rf ~/.tmux > /dev/null 2>&1
   if [ "$(uname)" = "Darwin" ]; then
+    sudo rm -rf ~/dotfiles/system_wide_emacs_movement.dict > /dev/null 2>&1
   else
     sudo rm -rf ~/.Xresources > /dev/null 2>&1
     sudo rm -rf ~/.xinitrc > /dev/null 2>&1
@@ -35,7 +38,7 @@ then
     ln -s ~/dotfiles/system_wide_emacs_movement.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
 
     # by default Terminal.app doesn't read .bashrc, but .profile works
-    echo source ~/.bashrc > ~/.profile
+    echo 'source ~/.bashrc' > ~/.profile
   else
     ln -s ~/dotfiles/bash_linux ~/.bash_linux
     ln -s ~/dotfiles/Xresources ~/.Xresources
