f3d4f7f68c086c11030e8cc0eea7876571a428cc
boris <boris@verhovs.ky>
2018-08-26 09:33:08 -0600

    made decimal time zero indexed

--- tmux/mac_tmux/scripts/decimal_time.sh
+++ tmux/mac_tmux/scripts/decimal_time.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# number of days since start of year
-dayofyear=$(date +%j | sed 's/^0*//') # sed strips the leading 0
+# number of days since start of year, zero indexed
+# january 1st is "0"
+dayofyear=$(printf "%d\n" $(($(date +%j)-1)))
 # returns .5000 for noon
-fractionofday=$(echo $(date +%k) / 24 + $(date +%M) / 1440 + $(date +%S) / 86400 | bc -l | cut -c1-5 | sed 's/^1*//') # sed strips the leading 1, just in case
+# fractionofday=$(echo $(date +%k) / 24 + $(date +%M) / 1440 + $(date +%S) / 86400 | bc -l | cut -c1-5 | sed 's/^1*//') # sed strips the leading 1, just in case
+
+# returns 5000 for noon
+fractionofday=$(echo $(date +%k) / 24 + $(date +%M) / 1440 + $(date +%S) / 86400 | bc -l | cut -c1-5 | sed 's/^1\.*//') # sed strips the leading 1 and decimal and replaces , just in case
 
 echo $dayofyear$fractionofday
 
