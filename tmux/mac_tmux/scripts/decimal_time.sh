0fb9d1a476870f2c5e56f03bba3791dc30ada9c7
boris <boris@verhovs.ky>
2020-07-14 15:51:46 -0400

    Fix tmux dates

--- tmux/mac_tmux/scripts/decimal_time.sh
+++ tmux/mac_tmux/scripts/decimal_time.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+year=$(printf "%05d" $(date +%Y | sed 's/^0*//'))
 # number of days since start of year, zero indexed
 # january 1st is "0"
-dayofyear=$(printf "%d\n" $(($(date +%j)-1)))
+# `date +%j` returns a three digit number, with leading zeros which I have to strip
+dayofyear=$(printf "%d" $(($(date +%j | sed 's/^0*//')-1)))
 # returns .5000 for noon
 # fractionofday=$(echo $(date +%k) / 24 + $(date +%M) / 1440 + $(date +%S) / 86400 | bc -l | cut -c1-5 | sed 's/^1*//') # sed strips the leading 1, just in case
 
 # returns 5000 for noon
 fractionofday=$(echo $(date +%k) / 24 + $(date +%M) / 1440 + $(date +%S) / 86400 | bc -l | cut -c1-5 | sed 's/^1\.*//') # sed strips the leading 1 and decimal and replaces, just in case
 
-echo $dayofyear$fractionofday
-
-
+echo $year $dayofyear$fractionofday
