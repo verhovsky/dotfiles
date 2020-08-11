4ad1c3766fe158a13a39f2c6521ef0ccd5692c1c
boris <boris.verk@gmail.com>
2020-08-11 00:24:57 -0600

    disable smart parens in emacs

--- config/doom/config.el
+++ config/doom/config.el
@@ -1,13 +1,20 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 (setq doom-theme 'doom-one-light)
-(set-frame-font "Source Code Pro")
+(setq doom-font (font-spec :family "Source Code Pro" :size 18))
+;; start in fullscreen
+(add-to-list 'initial-frame-alist '(fullscreen . fullboth))
 (setq python-shell-interpreter "python3")
+
 ;; don't walk back character when exiting insert mode
 (setq evil-move-cursor-back nil)
+
 ;; don't treat underscores as word delimeters
-(modify-syntax-entry ?_ "w")
+(defalias 'forward-evil-word 'forward-evil-symbol)
 (add-hook! 'js2-mode-hook (modify-syntax-entry ?_ "w")) ;; because the above doesn't work for JS
+
+;; don't automatically add parentheses and quotes
+(remove-hook 'doom-first-buffer-hook #'smartparens-global-mode)
 
 ;; (defun pyupgrade ()
 ;;   (interactive)
@@ -15,7 +22,7 @@
 
 (defun pyupgrade ()
   (interactive)
-  (shell-command (format "pyupgrade --py37-plus %S" buffer-file-name))
+  (shell-command (format "pyupgrade --py38-plus %S" buffer-file-name))
   (revert-buffer nil t))
 
 ;; (add-hook! 'python-mode-hook
